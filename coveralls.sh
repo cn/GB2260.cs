d353095a9975e4fddbc6629c3219c97d8a4e119e
codeyu <codeyu@gmail.com>
2017-08-12 00:11:01 +0800

    update converalls.sh

--- coveralls.sh
+++ coveralls.sh
@@ -3,5 +3,9 @@
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
   nuget install -OutputDirectory packages -Version 1.3.4 coveralls.io
-  packages\coveralls.io.1.3.4\tools\coveralls.net.exe --opencover -i coverage/coverage.xml --useRelativePaths
+  COVERALLSNET=$PWD/packages/coveralls.io.1.3.4/tools/coveralls.net.exe
+  $COVERALLSNET \
+    --opencover \
+    -i coverage/coverage.xml \
+    --useRelativePaths
 fi
