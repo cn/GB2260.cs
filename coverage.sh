9fb424d20b628f502ef80c1164b48075656c9a8b
codeyu <codeyu@gmail.com>
2017-08-12 00:29:36 +0800

    fix coveralls.io

--- coverage.sh
+++ coverage.sh
@@ -50,3 +50,13 @@ $REPORTGENERATOR \
   -reports:$coverage/coverage.xml \
   -targetdir:$coverage \
   -verbosity:Error
+
+echo "Submitting coverage statistics to coveralls.io"
+if [ -n "$COVERALLS_REPO_TOKEN" ]
+then
+  nuget install -OutputDirectory packages -Version 1.3.4 coveralls.io
+  COVERALLSNET=$PWD/packages/coveralls.io.1.3.4/tools/coveralls.net.exe
+  $COVERALLSNET \
+    --opencover $coverage/coverage.xml \
+    -r $COVERALLS_REPO_TOKEN
+fi
