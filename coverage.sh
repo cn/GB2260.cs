aa7e83f98990426db8305bd59318539f2278164c
codeyu <codeyu@gmail.com>
2017-08-12 01:07:18 +0800

    wait

--- coverage.sh
+++ coverage.sh
@@ -43,20 +43,10 @@ $OPENCOVER \
   -oldStyle \
   -filter:"+[*]*" \
   -searchdirs:$testdir/bin/$CONFIG/netcoreapp1.1 \
-  -register:Path32
+  -register:user
 
 echo "Generating HTML report"
 $REPORTGENERATOR \
   -reports:$coverage/coverage.xml \
   -targetdir:$coverage \
   -verbosity:Error
-
-echo "Submitting coverage statistics to coveralls.io"
-if [ -n "$COVERALLS_REPO_TOKEN" ]
-then
-  nuget install -OutputDirectory packages -Version 1.3.4 coveralls.io
-  COVERALLSNET=$PWD/packages/coveralls.io.1.3.4/tools/coveralls.net.exe
-  $COVERALLSNET \
-    --opencover $coverage/coverage.xml \
-    -r $COVERALLS_REPO_TOKEN
-fi
--- coveralls.sh
+++ coveralls.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "Submitting coverage statistics to coveralls.io"
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
   nuget install -OutputDirectory packages -Version 1.3.4 coveralls.io
   COVERALLSNET=$PWD/packages/coveralls.io.1.3.4/tools/coveralls.net.exe
+  coverage=./coverage
   $COVERALLSNET \
-    --opencover coverage/coverage.xml \
+    --opencover $coverage/coverage.xml \
     -r $COVERALLS_REPO_TOKEN
 fi
