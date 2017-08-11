ade63c2de123ff7d5c2c0430486604261e02557c
codeyu <codeyu@gmail.com>
2017-08-11 23:58:03 +0800

    update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -31,18 +31,18 @@ coverage=./coverage
 rm -rf $coverage
 mkdir $coverage
 
-dotnet test -f netcoreapp1.0 $DOTNET_TEST_ARGS test/GB2260.Test/GB2260.Test.csproj
+dotnet test -f netcoreapp1.1 $DOTNET_TEST_ARGS test/GB2260.Test/GB2260.Test.csproj
 
 echo "Calculating coverage with OpenCover"
 $OPENCOVER \
   -target:"c:\Program Files\dotnet\dotnet.exe" \
-  -targetargs:"test -f netcoreapp1.0 $DOTNET_TEST_ARGS test/GB2260.Test/GB2260.Test.csproj" \
+  -targetargs:"test -f netcoreapp1.1 $DOTNET_TEST_ARGS test/GB2260.Test/GB2260.Test.csproj" \
   -mergeoutput \
   -hideskipped:File \
   -output:$coverage/coverage.xml \
   -oldStyle \
   -filter:"+[GB2260*]* -[GB2260.*Tests*]*" \
-  -searchdirs:$testdir/bin/$CONFIG/netcoreapp1.0 \
+  -searchdirs:$testdir/bin/$CONFIG/netcoreapp1.1 \
   -register:user
 
 echo "Generating HTML report"
